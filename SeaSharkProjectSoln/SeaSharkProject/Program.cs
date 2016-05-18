118eeb4625d94bca543554d70bd6724bf4643af4
Deji Akin <presideji@yahoo.com>
2016-05-18 19:26:02 +0100

    Treated Constructor properly

--- SeaSharkProjectSoln/SeaSharkProject/Program.cs
+++ SeaSharkProjectSoln/SeaSharkProject/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SeaSharkProject.calf;
+using SeaSharkProject.facebook;
 
 namespace SeaSharkProject
 {
@@ -12,8 +13,16 @@ namespace SeaSharkProject
 
         public static void Main(string[] args)
         {
-            
+            //Elephant elephant = new Elephant();
+            Elephant elephant = new Elephant("Edwin", "Loveth", "Anney", "Hitler");
+            //now lets go back to elephant in facebook--Elephant
 
+            string name =  elephant.GetFirstStudentName();
+            Console.WriteLine(name);
+
+            Elephant pigeon = new Elephant("Avan Rolf", "Jeff Morgan", "Roy Fielding", "David cameron");
+            string name1 = pigeon.GetFirstStudentName();
+            Console.WriteLine(name1);
 
         }
     }
---
+++ SeaSharkProjectSoln/SeaSharkProject/facebook/Elephant.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaSharkProject.facebook
+{
+    public class Elephant
+    {
+        //looking at constructors. now lets look at what constructors do that 
+        //i do not like. lets assign a value to the string.
+        private string _firstStudentName;
+        private string _secondStudentName;
+        private string _thirdStudentName;
+        private string _fourthStudentName;
+        //now lets go and instanciate elephant. lick on Program.cs
+
+
+        //public Elephant()
+        //if we don't know the name we can code as below
+        public Elephant(string firststudentName, string secondStudentName, string thirdStudentname, string fourthStudentName)
+        {
+            //  _firstStudentName = "Andre";
+            //  _secondStudentName = "Bukky";
+            //  _thirdStudentName = "JohnBull";
+            //  _fourthStudentName = "Johnson";
+            _firstStudentName = firststudentName;
+            _secondStudentName = secondStudentName;
+            _thirdStudentName = thirdStudentname;
+            _fourthStudentName = fourthStudentName;
+
+        }
+        public string GetFirstStudentName()
+
+        {
+            return _firstStudentName;
+            //it will pick value in the string and return it.
+            //lets go to Program,cs now
+
+
+
+        }
+
+    }
+}
